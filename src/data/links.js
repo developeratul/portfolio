881a4979c4aadf85eac3e82edefdd17629caa0a1
Ratul <azammmgol@gmail.com>
2021-09-09 16:30:36 +0600

    updated my linkedin profile url

--- src/data/links.js
+++ src/data/links.js
@@ -1,7 +1,7 @@
 const links = {
   facebook: "https://www.facebook.com/minhazurrahaman.ratul/",
   github: "https://github.com/ratul-devr",
-  linkedin: "https://www.linkedin.com/in/developer-ratul-407352211/",
+  linkedin: "https://www.linkedin.com/in/minhazur-rahman-ratul-407352211/",
   twitter: "https://twitter.com/Ratul_devR",
 };
 
