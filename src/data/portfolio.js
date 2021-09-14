9359bd7e04e779119b9a6419748004056374815a
Ratul <azammmgol@gmail.com>
2021-09-14 17:23:58 +0600

    removed one project

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,24 +1,15 @@
 const portfolioData = [
   {
     id: 1,
-    title: "DevR Media",
-    desc: "A social media application with CRUD functionalities 🔥💯",
-    img: "https://res.cloudinary.com/devr-static-assets-db/image/upload/v1628070110/DevR-portfolio/projects/devr_media_ma2wd3.png",
-    repo: "https://github.com/ratul-devR/devr-dev-media",
-    demo: "https://dev-media-devr.herokuapp.com/",
-    animation: "fade-right",
-  },
-  {
-    id: 2,
     title: "Meme Site",
     desc: "A meme sharing platform created using the MERN stack with TS",
     img: "https://res.cloudinary.com/devr-static-assets-db/image/upload/v1631618230/DevR-portfolio/projects/Web_capture_14-9-2021_171142_devr-memes.herokuapp.com_kewozc.jpg",
     repo: "https://github.com/ratul-devR/meme-website",
     demo: "https://devr-memes.herokuapp.com/",
-    animation: "fade-up",
+    animation: "fade-right",
   },
   {
-    id: 3,
+    id: 2,
     title: "Music Player",
     desc: "Ultimate music player made with vanilla Javascript",
     img: "https://res.cloudinary.com/devr-static-assets-db/image/upload/v1628070628/DevR-portfolio/projects/music_player_atw4v7.png",
@@ -27,7 +18,7 @@ const portfolioData = [
     animation: "fade-left",
   },
   {
-    id: 4,
+    id: 3,
     title: "Landing Page",
     desc: "A responsive portfolio template with React and Sass",
     img: "https://res.cloudinary.com/devr-static-assets-db/image/upload/v1628070754/DevR-portfolio/projects/portfolio_landing_page_krldgd.png",
@@ -36,7 +27,7 @@ const portfolioData = [
     animation: "fade-right",
   },
   {
-    id: 5,
+    id: 4,
     title: "Todo App",
     desc: "A todo app using Next and Typescript",
     img: "https://res.cloudinary.com/devr-static-assets-db/image/upload/v1630042486/DevR-portfolio/projects/todo-app_jp1d3u.png",
@@ -45,7 +36,7 @@ const portfolioData = [
     animation: "fade-up",
   },
   {
-    id: 6,
+    id: 5,
     title: "Calculator",
     desc: "A calculator using Next and Typescript",
     img: "https://res.cloudinary.com/devr-static-assets-db/image/upload/v1630042438/DevR-portfolio/projects/calculator_etsd51.png",
