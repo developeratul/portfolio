5c9329d2d4afcd2fe15bcc890da91e7ba264b5f7
DevR <azammmgol@gmail.com>
2022-12-17 21:18:39 +0600

    Done with projects section

--- components/Home/Projects.tsx
+++ components/Home/Projects.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ArrowTopRightOnSquareIcon, CodeBracketIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 
 import { Button, Section } from "@/components";
@@ -25,7 +26,7 @@ const projectData: Project[] = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempore error quod ea consectetur esse, tenetur et laboriosam nisi numquam!",
     imageURL: "/devr_commerce.webp",
-    stack: ["Next Js", "TypeScript", "MUI"],
+    stack: ["Next Js", "MUI", "Next Js"],
     preview: "http://google.com",
     repo: "http://github.com",
   },
@@ -56,33 +57,50 @@ const projectData: Project[] = [
 export function Project(props: ProjectProps) {
   const { name, description, id, imageURL, category, stack, preview, repo } = props;
   return (
-    <div id={id} className="flex flex-col gap-5 rounded-md p-5 shadow-md dark:bg-background-400">
+    <div
+      id={id}
+      className="flex flex-col gap-5 rounded-md bg-white p-5 shadow-md dark:bg-background-400"
+    >
       <Image
         src={imageURL}
         alt={name}
         width={500}
         height={500}
-        className="aspect-video w-full rounded-md object-cover"
+        className="aspect-[2/1] w-full rounded-md object-cover"
       />
       <div className="flex flex-col gap-3">
         <div>
-          <p className="mb-[2px] text-sm uppercase dark:text-primary-500">{category}</p>
+          <p className="mb-[2px] text-sm uppercase text-primary-600 dark:text-primary-500">
+            {category}
+          </p>
           <h2 className="text-2xl ">{name}</h2>
         </div>
-        <div className="flex gap-3">
+        <div className="grid grid-cols-3 gap-3 lg:grid-cols-4">
           {stack.map((tech, index) => (
             <span
               key={index}
-              className="inline-flex flex-auto items-center justify-center rounded-md py-1 px-3 align-middle text-xs dark:bg-primary-500 dark:text-primary-800"
+              className="inline-flex flex-auto items-center justify-center rounded-md bg-primary-600 py-1 px-3 text-center align-middle text-xs text-primary-50 dark:bg-primary-500 dark:text-primary-800"
             >
               {tech}
             </span>
           ))}
         </div>
-        <p className="text-sm dark:text-gray-100">{description}</p>
+        <p className="text-sm text-gray-600 dark:text-gray-100">{description}</p>
         <div className="flex gap-3">
-          {preview && <Button size="sm">Preview</Button>}
-          {repo && <Button size="sm">Github</Button>}
+          {preview && (
+            <a href={preview} target="_blank" rel="noreferrer">
+              <Button size="sm" startIcon={<ArrowTopRightOnSquareIcon width={18} height={18} />}>
+                Preview
+              </Button>
+            </a>
+          )}
+          {repo && (
+            <a href={repo} target="_blank" rel="noreferrer">
+              <Button size="sm" startIcon={<CodeBracketIcon width={18} height={18} />}>
+                Github
+              </Button>
+            </a>
+          )}
         </div>
       </div>
     </div>
@@ -90,10 +108,9 @@ export function Project(props: ProjectProps) {
 }
 
 export function Projects() {
-  console.log({ projectData });
   return (
     <Section no="02" title="Projects">
-      <div className="grid gap-5">
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
         {projectData.map((project) => (
           <Project {...project} key={project.id} />
         ))}
